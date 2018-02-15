c6a8af590b5f3639fcb9f045ae6e027e0f0c7a48
Alberto Paz <alberto.paz@11paths.com>
2018-02-15 22:44:54 +0100

    Converted to react

---
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -0,0 +1,12 @@
+class AppointmentsList extends React.Component {
+  render() {
+    return(
+      <div>
+      {this.props.appointments.map(function(appointment) {
+        return (
+          <Appointment appointment={appointment}/>
+        )
+      })}
+      </div>
+    )}
+}
