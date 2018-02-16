a7db0d675c13c91eb567550a39b7224c68410556
Alberto Paz <alberto.paz@11paths.com>
2018-02-16 13:09:03 +0100

    Move state up

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,28 @@
 class AppointmentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onUserInput = props.onUserInput.bind(this);
+  }
+
+
+  handleChange(e) {
+    var name = e.target.name;
+    var obj = {};
+    obj[name] = e.target.value;
+    this.onUserInput(obj);
+  }
+
   render() {
     return(
       <div>
       <h2>Make a new appointment</h2>
       <form>
-      <input name='title' placeholder='Appointment Title' />
-      <input name='appt_time' placeholder='Date and Time' />
+      <input name='title' placeholder='Appointment Title' 
+        value={this.props.input_title}
+        onChange={this.handleChange.bind(this)}/>
+      <input name='appt_time' placeholder='Date and Time' 
+        value={this.props.input_appt_time}
+        onChange={this.handleChange.bind(this)}/>
       <input type='submit' value='Make Appointment' />
       </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,21 @@
 class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: this.props.appointments,
+      title: 'Standup but on table',
+      appt_time: 'Tomorrow at 10am'
+    };
+  }
+
   render() {
     return (
       <div>
-      <AppointmentForm />
-      <AppointmentsList appointments={this.props.appointments}/>
+      <AppointmentForm 
+        input_title={this.state.title} 
+        input_appt_time={this.state.appt_time}
+        onUserInput={(obj) => this.setState(obj)}/>
+      <AppointmentsList appointments={this.state.appointments}/>
       </div>
     )
   }
